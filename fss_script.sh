cad45b6844242a1785c93ce3005356117e4ec839
sdi2100006 <sdi2100006@di.uoa.gr>
2025-05-01 13:34:31 +0300

    bash script complete

--- fss_script.sh
+++ fss_script.sh
@@ -7,22 +7,38 @@ do
         p) PATH_NAME=$OPTARG;;
 
         c) COMMAND=$OPTARG;;
+
+        *) echo "Wrong call"
+            exit 1
+            ;;
     esac
 done
-echo "path: $PATH_NAME";
-echo "command: $COMMAND";
+
+if [ -z "$PATH_NAME" ] || [ -z "$COMMAND" ];
+    then echo "One or more aguments missing"
+    exit 1
+fi
 
 case $COMMAND in
+    listAll) ;;
+
+    listMonitored) ;;
+
+    listStoped) ;;
+
     purge)  if [ -e "$PATH_NAME" ]; 
                 then if [ -f "$PATH_NAME" ];
-                    then echo " would run rm -f $PATH_NAME"
-                    #rm -f "$PATH_NAME"
+                    then echo "Deleting $PATH_NAME"
+                    rm -f "$PATH_NAME"
+                    echo "Purge coplete."
                 elif [ -d "$PATH_NAME" ];
-                    then echo "would run rm -rf $PATH_NAME"
-                    #rm -rf "$PATH_NAME"
+                    then echo "Deleting $PATH_NAME"
+                    rm -rf "$PATH_NAME"
+                    echo "Purge complete."
                 fi
             else
                 echo "File or directory doesnt exist"
             fi
             ;;
+    *) echo "Command doesnt exist";;
 esac
